da0258d4cc8cd6c98882a5c6a66d44aaa63705e8
Lakmal256 <lakmalfriends@gmail.com>
2022-12-21 23:01:46 +0530

    form_using_map

---
+++ src/new.js
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+import "./style.css";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import LogoutIcon from "@mui/icons-material/Logout";
+
+const App = () => {
+  const [formValues, setFormValues] = useState([{ question: "" }]);
+
+  const addFormFields = () => {
+    setFormValues([...formValues, { question: "" }]);
+  };
+
+  const removeFormFields = (i) => {
+    const newFormValues = [...formValues];
+    newFormValues.splice(i, 1);
+    setFormValues(newFormValues);
+  };
+
+  return (
+    <form>
+      {formValues.map((index) => (
+        <div className="form-inline" key={index}>
+          <label>Question</label>
+          <input type="text" name="name" />
+          {index ? (
+            <ListItemIcon
+              className="button remove"
+              onClick={() => removeFormFields(index)}
+            >
+              <LogoutIcon />
+            </ListItemIcon>
+          ) : null}
+        </div>
+      ))}
+      <div className="button-section">
+        <button
+          className="button add"
+          type="button"
+          onClick={() => addFormFields()}
+        >
+          Add
+        </button>
+      </div>
+    </form>
+  );
+};
+
+export default App;
